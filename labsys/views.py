e76eb7f56d203a2bace0f5eee1f0421be2cdd587
Brijesh Patel <bijupat@yahoo.com>
2021-05-18 13:48:09 +0530

    templates index and base added

--- labsys/views.py
+++ labsys/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the LABSYS index.")
+    
+    return render(request, 'labsys/index.html')
