1638c0af6e6b5145b8d972855bc29bb70911b61b
Brijesh Patel <bijupat@yahoo.com>
2021-06-09 13:31:12 +0530

    gitignore changed to ignore migration, database, settings.py

--- softlab/settings.py
+++ softlab/settings.py
@@ -73,7 +73,13 @@ WSGI_APPLICATION = 'softlab.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -84,6 +90,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+"""
 #Substituting a custom User model
 
 AUTH_USER_MODEL="labsys.User"
