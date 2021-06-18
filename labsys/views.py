64583e868c8cb6dc67c767af08a2273f87c1e7e0
Brijesh Patel <bijupat@yahoo.com>
2021-06-18 15:38:55 +0530

    date select form in index. html and correspoing changes in view.index

--- labsys/views.py
+++ labsys/views.py
@@ -9,43 +9,16 @@ from django.urls import reverse
 
 
 
-# Create your views here.
-
-
 def index(request):
     if request.method == 'GET':
         #date format (yyyy,mm,dd)
         encounter_today = Encounter.objects.filter(timedate__date=datetime.today().date())
 
-        """
-        encounter_date = Encounter.objects.filter(timedate__date=datetime(2021, 6, 2).date())
-        print(encounter_today)
-        for e in encounter_today:
-            #e.test.all().(total=sum('price'))
-            p = e.test.all().aggregate(Sum('price'))
-            print(p)
-        #create empty list to pass to template as value dic
-        context = []
-        for e in encounter_today:
-            #create dic item for each encounter
-            object = {}
-            #add value to object dict with different key 
-            object["date"] = e.timedate.date()
-            object["patient"] = e.patient.patient_name.get().text +" "+ e.patient.patient_name.get().family
-            object["id"] = e.id
-            object["practitioner"] = e.practitioner
-            object["account"] = e.account
-            object["test"] = e.test.all()
-            
-
-            #add object dict to list context
-            context.append(object)
-        print(context)
-        """
         return render(request, 'labsys\index.html', {"encounter" :encounter_today})
+
     elif request.method == 'POST':
-        
-        encounter_date = Encounter.objects.filter(timedate__date=datetime.today().date())
+        date = request.POST["date"]
+        encounter_date = Encounter.objects.filter(timedate__date=date.date())
 
         return render(request, 'labsys\index.html', {"encounter" :encounter_date})
 
